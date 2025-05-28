decf8d24614afb0e3513c74a58a81b3c8094f21a
mmshooreshi <mmshooreshi@gmail.com>
2025-05-28 12:12:02 +0330

    feat: register page greatly improvbed

---
+++ pages/enamad.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="verify">
+        <h1>35776297</h1>
+    </div>
+
+
+</template>
+<script setup>
+useHead({
+    title: '35776297',
+    meta: [
+        { name: 'enamad', content: '35776297' }
+    ]
+})
+
+</script>
