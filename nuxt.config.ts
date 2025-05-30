57af5a5ba5bc5cc3554649da68dfba5b7ecd6289
mmshooreshi <mmshooreshi@gmail.com>
2025-05-30 12:21:45 +0330

    fix: baileys version changed.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,6 +17,10 @@ if (fs.existsSync(pluginPath)) fs.rmSync(pluginPath, { force: true });
 export default defineNuxtConfig({
   app: {
     head: {
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' }
+      ],
+
       link: [
         {
           rel: 'icon',
